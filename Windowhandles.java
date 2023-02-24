b6d21a6e3912d9eb6dd571f44878f826c41f9650
ishwar chavan / letmecode <118907819+letmecode111@users.noreply.github.com>
2023-02-24 16:47:29 +0530

    Add files via upload
    handling multiple windows using get window handles .

--- Windowhandles.java
+++ Windowhandles.java
@@ -11,12 +11,20 @@ public class Windowhandles  extends Apputils{
 	public static void main(String[] args) {
 		
 		utils.Apputils.launchapp("http://gmail.com");
-		 String currentwindow=  driver.getWindowHandle();
-		System.out.println(currentwindow);
+		// String currentwindow=  driver.getWindowHandle();
+		//System.out.println(currentwindow);
 		driver.findElement(By.linkText("Help")).click();
 	Set<String>	 windowha=driver.getWindowHandles();
-		System.out.println(windowha);
-
+	Object [] windows=	windowha.toArray();
+	String window1= windows[0].toString();
+	   String   window2= windows[1].toString();
+	System.out.println(driver.getTitle());
+	  driver.switchTo().window(window2);
+	String title=  driver.switchTo().window(window1).getTitle();
+	   System.out.println(title);
 	}
+	
+	
+	
 
 }
